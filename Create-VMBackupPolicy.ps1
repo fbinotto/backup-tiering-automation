e97a37cbd20313c834228b39b0c2a92e93b83530
fbinotto <felipe_binotto@hotmail.com>
2022-06-03 16:32:48 +1000

    Initial commit

--- Create-VMBackupPolicy.ps1
+++ Create-VMBackupPolicy.ps1
@@ -9,19 +9,19 @@ $schPolEnhanced.ScheduleRunFrequency = 'Hourly'
 $Dt = Get-Date
 $Dt1 = (Get-Date -Year $Dt.Year -Month $Dt.Month -Day $Dt.Day -Hour $Dt.Hour -Minute 0 -Second 0 -Millisecond 0).ToUniversalTime()
 
-# TIER_1 Schedule
+# TIER-1 Schedule
 $T1HourlySchedule = New-Object Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.Models.HourlySchedule
 $T1HourlySchedule.Interval = 4
 $T1HourlySchedule.WindowStartTime = $dt1
 $T1HourlySchedule.WindowDuration = 24
 
-# TIER_2 Schedule
+# TIER-2 Schedule
 $T2HourlySchedule = New-Object Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.Models.HourlySchedule
 $T2HourlySchedule.Interval = 6
 $T2HourlySchedule.WindowStartTime = $dt1
 $T2HourlySchedule.WindowDuration = 24
 
-# TIER_3 Schedule
+# TIER-3 Schedule
 $T3HourlySchedule = New-Object Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.Models.HourlySchedule
 $T3HourlySchedule.Interval = 12
 $T3HourlySchedule.WindowStartTime = $dt1
@@ -39,23 +39,23 @@ foreach ($sub in $subscriptions) {
 
 	  $retPol.DailySchedule.DurationCountInDays = 90
         $schPolEnhanced.HourlySchedule = $T1HourlySchedule
-        $ProtectionPolicy = New-AzRecoveryServicesBackupProtectionPolicy -Name TIER_1 `
+        $ProtectionPolicy = New-AzRecoveryServicesBackupProtectionPolicy -Name TIER-1 `
             -WorkloadType AzureVM -RetentionPolicy $retPol -SchedulePolicy $schPolEnhanced -VaultId $vault.Id
 
 	  $retPol.DailySchedule.DurationCountInDays = 60
         $schPolEnhanced.HourlySchedule = $T1HourlySchedule
-        $ProtectionPolicy = New-AzRecoveryServicesBackupProtectionPolicy -Name TIER_2 `
+        $ProtectionPolicy = New-AzRecoveryServicesBackupProtectionPolicy -Name TIER-2 `
             -WorkloadType AzureVM -RetentionPolicy $retPol -SchedulePolicy $schPolEnhanced -VaultId $vault.Id
 
 	  $retPol.DailySchedule.DurationCountInDays = 30
         $schPolEnhanced.HourlySchedule = $T3HourlySchedule
-        $ProtectionPolicy = New-AzRecoveryServicesBackupProtectionPolicy -Name TIER_3 `
+        $ProtectionPolicy = New-AzRecoveryServicesBackupProtectionPolicy -Name TIER-3 `
             -WorkloadType AzureVM -RetentionPolicy $retPol -SchedulePolicy $schPolEnhanced -VaultId $vault.Id
 
 	  $retPol.DailySchedule.DurationCountInDays = 15
         $schPolDefault.ScheduleRunTimes.Clear()
         $schPolDefault.ScheduleRunTimes.Add($dt1)
-        $ProtectionPolicy = New-AzRecoveryServicesBackupProtectionPolicy -Name TIER_4 `
+        $ProtectionPolicy = New-AzRecoveryServicesBackupProtectionPolicy -Name TIER-4 `
             -WorkloadType AzureVM -RetentionPolicy $retPol -SchedulePolicy $schPolDefault -VaultId $vault.Id
 
     }
